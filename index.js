66db252689785eca53a1980fb095f6b307d72827
Anthony <kabuthuantony@gmail.com>
2024-03-20 22:29:18 +0300

    JS

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+// helpers.js
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 20;
+const num4 = 4;
+const mod = num3 % num4; // This calculates the remainder when num3 is divided by num4
+
+const max = Math.max(5, 10, 15, 20);
+
+module.exports = { num1, num2, multiply, random, num3, num4, mod, max };
